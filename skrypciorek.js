8739b1cb727554c743a4596d70e44bf10a5934b3
AbsoluteCrest <piotrek.ruzanski@gmail.com>
2023-12-06 21:59:25 +0100

    nowosci tak o

--- skrypciorek.js
+++ skrypciorek.js
@@ -1,8 +1,13 @@
 function pobieranie() {
-    let a = document.getElementById('lol')
-    a = a.value
+
+    let a = document.getElementById('lol').value
     console.log(a)
+    document.querySelector('#op1').innerHTML = `${a}`
+    
 }
+
+
+//const pobieranie = ()=>{} inny zapis funkcji
 
 
 
