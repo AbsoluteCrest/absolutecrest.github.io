b6e689d0a65741fe56c2a48cc215a407001a5ef5
AbsoluteCrest <piotrek.ruzanski@gmail.com>
2024-05-21 21:23:42 +0200

    Gowno zrobione

---
+++ res.js
@@ -0,0 +1,11 @@
+$('document').ready(function(){
+    $('#close').click(function(){
+        $('#sidebar').hide();
+    });
+    $('#show').click(function(){
+        $('#sidebar').show();
+    });
+});
+
+
+
