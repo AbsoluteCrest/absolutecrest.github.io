788f00e37d2d2eaeea9bd198bd3c5e049518e17d
Crest <piotrek.ruzanski@gmail.com>
2024-11-29 21:23:45 +0100

    git workflows added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import NavBar from "@/app/components/NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body
           className={`${geistSans.variable} ${geistMono.variable} ${poppinsRegular.variable}antialiased`}
       >
+      //<NavBar />
       {children}
 
       </body>
