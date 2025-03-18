b2f4ead01e03fffaa79d1a78c828a89d717478c5
Crest <piotrek.ruzanski@gmail.com>
2025-03-18 21:19:55 +0100

    footer changed

--- src/app/components/MainPage.tsx
+++ src/app/components/MainPage.tsx
@@ -1,7 +1,7 @@
 const MainPage = () => {
     return (
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            <div className="  rounded-lg p-6 bg-neutral-900 font-sfpro font-bold text-4xl">
+            <div className="  rounded-lg p-6 bg-neutral-900 font-sfpro font-bold text-4xl animate-fade-up">
                 <p className="font-sfpro text-4xl mb-3">Absolute Crest</p>
                 <p className="font-sfpro ">main</p>
             </div>
